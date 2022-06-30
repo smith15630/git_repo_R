fd2e6bf63490e3f7fab00efa48960a79b8172785
smith15630 <smith15630@buckeyemail.osu.edu>
2022-06-30 16:50:14 +0000

    Adding updated files

--- r assignment.R
+++ r assignment.R
@@ -1,5 +1,7 @@
 reg<-read.csv("regrex1.csv")
-plot(reg$y~reg$x, main="regression", xlab="x", ylab="y")
+plot(reg$y~reg$x, main="plot", xlab="x", ylab="y", pch=19, col="goldenrod")
 lm(formula=reg$y~reg$x)
+
+plot(reg$y~reg$x, main="plot with regression", xlab="x", ylab="y", pch=19, col="salmon")
 model<-lm(formula=reg$y~reg$x)
 abline(model)
